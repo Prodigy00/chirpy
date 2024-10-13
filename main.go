260f3d8ca465cbcbe4c82d8788769373a648a09a
Gideon Idowu <simplygiddy@gmail.com>
2024-10-13 02:41:08 +0100

    add ability to record and reset server traffic

--- main.go
+++ main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func NewApiConfig() *apiConfig {
+	return &apiConfig{}
+}
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	incSrvHits := func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(incSrvHits)
+}
+
+func (cfg *apiConfig) Reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) FileServerHits(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
 
 func main() {
 	ns := http.NewServeMux()
 	fs := http.FileServer(http.Dir("."))
-
+	c := NewApiConfig()
 	stripped := http.StripPrefix("/app/", fs)
 
-	ns.Handle("/app/", stripped)
+	ns.Handle("/app/", c.middlewareMetricsInc(stripped))
 	ns.Handle("/app/assets/logo.png", stripped)
 
 	ns.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +48,8 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
+	ns.HandleFunc("/metrics", c.FileServerHits)
+	ns.HandleFunc("/reset", c.Reset)
 	server := http.Server{
 		Handler: ns,
 		Addr:    ":8080",
